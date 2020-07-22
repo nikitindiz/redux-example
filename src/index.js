bd1b69c3a7572e91eb725336ed299b6cff5512b6
Alexander Nikitin <al.nikitin@andersenlab.com>
2020-07-22 13:28:42 +0300

    Redux store implemented

--- src/index.js
+++ src/index.js
@@ -1,11 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App';
 
+const counterReducer = (
+  state = { counter: 0 },
+  action,
+) => {
+  const { type } = action;
+
+  if (type === 'INCREASE') {
+    return {
+      counter: state.counter + 1
+    };
+  }
+
+  if (type === 'DECREASE') {
+    return {
+      counter: state.counter - 1
+    };
+  }
+
+  return state;
+};
+
+const rootReducer = combineReducers({
+  counterReducer,
+  // here can be more reducers
+});
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
